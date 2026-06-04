57d2e0a1d1199d25afb4341dd7c1d5e94931c370
PRIyanshu33-99 <py2199763@gmail.com>
2026-06-04 20:31:37 +0530

    Add files via upload
    day8_aasignment

---
+++ day8_assignment.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+public class day8_assignment {
+    public static void main(String[] args) {
+
+        // Q29 - Half Pyramid Pattern
+        System.out.println("Q29 - Half Pyramid Pattern");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Q30 - Number Triangle
+        System.out.println("Q30 - Number Triangle");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Q31 - Character Triangle
+        System.out.println("Q31 - Character Triangle");
+        for (char i = 'A'; i <= 'E'; i++) {
+            for (char j = 'A'; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Q32 - Repeated Number Pattern
+        System.out.println("Q32 - Repeated Number Pattern");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+  
+   
+   
+   
+    }
+} 
+    
+
