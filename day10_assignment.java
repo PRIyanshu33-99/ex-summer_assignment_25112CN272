08c60082f213bf6849d8ec9e9882653e9a195c76
PRIyanshu33-99 <py2199763@gmail.com>
2026-06-11 14:15:04 +0530

    Refactor star and number pyramid implementations

--- day10_assignment.java
+++ day10_assignment.java
@@ -1,50 +1,72 @@
-public class day10_assignment {
+import java.util.Scanner;
+
+public class practiceset_2 {
+
     public static void main(String[] args) {
 
-        // Q37 Star Pyramid
-        System.out.println("Star Pyramid");
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= (2 * i - 1); j++) {
+        int n = 5;
+        int i, j, k;
+
+        // 1. Star Pyramid
+        System.out.println("Star Pyramid:");
+        for (i = 1; i <= n; i++) {
+
+            for (j = 1; j <= n - i; j++)
+                System.out.print(" ");
+
+            for (k = 1; k <= 2 * i - 1; k++)
                 System.out.print("*");
-            }
+
             System.out.println();
         }
 
-        // Q38 Reverse Pyramid
-        System.out.println("\nReverse Pyramid");
-        for (int i = 5; i >= 1; i--) {
-            for (int j = 1; j <= (2 * i - 1); j++) {
+        // 2. Reverse Star Pyramid
+        System.out.println("\nReverse Star Pyramid:");
+        for (i = n; i >= 1; i--) {
+
+            for (j = 1; j <= n - i; j++)
+                System.out.print(" ");
+
+            for (k = 1; k <= 2 * i - 1; k++)
                 System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        // Q39 Number Pyramid
-        System.out.println("\nNumber Pyramid");
-        for (int i = 1; i <= 5; i++) {
-
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j);
-            }
-
-            for (int j = i - 1; j >= 1; j--) {
-                System.out.print(j);
-            }
 
             System.out.println();
         }
 
-        // Q40 Character Pyramid
-        System.out.println("\nCharacter Pyramid");
-        for (int i = 1; i <= 5; i++) {
+        // 3. Number Pyramid
+        System.out.println("\nNumber Pyramid:");
+        for (i = 1; i <= n; i++) {
 
-            for (char ch = 'A'; ch <= ('A' + i - 1); ch++) {
-                System.out.print(ch);
-            }
+            for (j = 1; j <= n - i; j++)
+                System.out.print(" ");
 
-            for (char ch = (char) ('A' + i - 2); ch >= 'A'; ch--) {
-                System.out.print(ch);
-            }
+            for (j = 1; j <= i; j++)
+                System.out.print(j);
+
+            for (j = i - 1; j >= 1; j--)
+                System.out.print(j);
+
+            System.out.println();
+        }
+
+        // 4. Character Pyramid
+        System.out.println("\nCharacter Pyramid:");
+        for (i = 1; i <= n; i++) {
+
+            char ch;
+
+            for (j = 1; j <= n - i; j++)
+                System.out.print(" ");
+
+            ch = 'A';
+
+            for (j = 1; j <= i; j++)
+                System.out.print(ch++);
+
+            ch -= 2;
+
+            for (j = 1; j < i; j++)
+                System.out.print(ch--);
 
             System.out.println();
         }
